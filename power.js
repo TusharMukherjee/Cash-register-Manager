43fac44c66b052b8dc844b7dd01877e76fd92455
Tushar Mukherjee <tushar1214mrt@gmail.com>
2021-01-06 04:17:55 +0530

    cashflow

--- power.js
+++ power.js
@@ -2,15 +2,17 @@ var totalp = document.querySelector("#tprice");
 var paidp = document.querySelector("#paid");
 var sub = document.querySelector("#mutton");
 var rupyaa = document.querySelector("#rupya");
-var ac = document.querySelector("#clear");
 
 var money = [2000, 500, 200, 100, 20, 10, 5, 1];
 
 var a = 0;
 
-
+function myFunction(){
+    rupyaa.innerText = "";
+}
 
 function process(total, paid){
+    myFunction();
     if(paid == "" || total == "" || paid < 0 || total < 0){
     try {
         if(total == "" || paid == "") throw "can't be empty";
@@ -23,18 +25,6 @@ function process(total, paid){
     else if(paid < total){
         rupyaa.innerText = "Sorry Paid amount is less then Total amount"
     }
-    // if(total < 0){
-    //     eror.innerHTML = "Total can't be negative";
-    // }
-    // else if(paid < 0){
-    //     eror.innerHTML = "Given amount can't be negative";
-    // }
-    // else if(total == ""){
-    //     eror.innerHTML = "Total can't be empty";
-    // }
-    // else if(paid == ""){
-    //     eror.innerHTML = "Given amount can't be empty";
-    // }
     else{
     var leftAmt = paid - total;
     for(var i = 0; i < 8; i++){
